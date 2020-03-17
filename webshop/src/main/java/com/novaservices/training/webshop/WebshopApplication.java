92eb5548702874ed857b3826b4ffc8823dfd6e50
Gabor Imre <gabor@aut.bme.hu>
2020-03-17 10:02:50 +0100

    comment test calls in command line runner

--- webshop/src/main/java/com/novaservices/training/webshop/WebshopApplication.java
+++ webshop/src/main/java/com/novaservices/training/webshop/WebshopApplication.java
@@ -24,11 +24,13 @@ public class WebshopApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		/*
 		productRepository.save(new Product("abc", 100.0));
 		productRepository.save(new Product("abc", 200.0));
 		productRepository.save(new Product("def", 200.0));
 		discountService.discountProductsByName("abc", 10);
 		productRepository.findAll().forEach(System.out::println);
+		*/
 	}
 
 }
