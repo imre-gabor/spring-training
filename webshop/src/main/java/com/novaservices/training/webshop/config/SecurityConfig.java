d824d8b259dca4b039aa8266cc03ce1c7f659b55
Gabor Imre <gabor@aut.bme.hu>
2020-04-02 09:31:42 +0200

    JWT token generálása login esetén

--- webshop/src/main/java/com/novaservices/training/webshop/config/SecurityConfig.java
+++ webshop/src/main/java/com/novaservices/training/webshop/config/SecurityConfig.java
@@ -3,17 +3,22 @@ package com.novaservices.training.webshop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.novaservices.training.webshop.security.JWTAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	
 	@Bean
 	public PasswordEncoder encoder() {
 	    return new BCryptPasswordEncoder();
@@ -33,13 +38,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception { 
 	    http
-	    .httpBasic()
+	    	.httpBasic()
 	    .and()
-	    .csrf().disable()
+	    	.csrf().disable()
 	    .authorizeRequests()
-	    .antMatchers(HttpMethod.GET, "/api/products/**").permitAll()
-	    .antMatchers(HttpMethod.POST, "/api/products").authenticated()
-	    .antMatchers(HttpMethod.POST, "/api/categories").hasRole("ADMIN")
-	    .antMatchers("/**").denyAll();
+		    .antMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+		    .antMatchers(HttpMethod.POST, "/api/products").authenticated()
+		    .antMatchers(HttpMethod.POST, "/api/categories").hasRole("ADMIN")
+		    .antMatchers("/**").denyAll()
+	    .and()
+	    	.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+	    	
+	    	.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	    ;
 	}
 }
---
+++ webshop/src/main/java/com/novaservices/training/webshop/security/JWTAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.novaservices.training.webshop.security;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	
+	private static final String BEARER = "Bearer ";
+	private static final String AUTHORIZATION = "Authorization";
+	private static final String JWT_SECRET = "mysecret";
+	private static final String AUTHORITIES = "authorities";
+	private AuthenticationManager authenticationManager;
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		
+		String username = request.getParameter("username");
+		String pass = request.getParameter("pass");
+		
+		return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, pass));
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, 
+			HttpServletResponse response, 
+			FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		
+		String jwt = JWT.create()
+			.withSubject(((UserDetails)authResult.getPrincipal()).getUsername())
+			.withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1)))
+			.withArrayClaim(AUTHORITIES, 
+					authResult.getAuthorities()
+					.stream()
+					.map(GrantedAuthority::getAuthority)
+					.collect(Collectors.toList()).toArray(new String[0]))
+			.sign(Algorithm.HMAC512(JWT_SECRET));
+		
+		response.addHeader(AUTHORIZATION, BEARER + jwt);
+	}
+	
+	
+
+}
